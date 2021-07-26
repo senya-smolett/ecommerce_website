46f0de000867949657c4f4520d72b829aedcd2ec
senya <ssmolett@gmail.com>
2021-07-26 19:04:38 -0400

    integrated node.js server

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,14 @@
-import express from "express";
-import data from "./data.js";
+import express from 'express';
+import data from './data.js';
 
 const app = express();
 
-app.get("/api/products", (req, res) => {
+app.get('/api/products', (req, res) => {
   res.send(data.products);
 });
 
-app.get("/", (req, res) => {
-  res.send("Server is ready");
+app.get('/', (req, res) => {
+  res.send('Server is ready');
 });
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
